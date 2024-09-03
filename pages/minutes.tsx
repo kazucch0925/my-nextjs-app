3720bce64f3ecb6cc1388f2fd2a527b84ac86f24
kazucch0925 <kazuto925.corocoro@hotmail.com>
2024-09-03 23:40:11 +0900

    pagesディレクトリ作成

---
+++ pages/minutes.tsx
@@ -0,0 +1,12 @@
+// src/app/minutes/page.tsx
+
+import React from 'react';
+import Minutes from '../src/components/Minutes';
+
+export default function MinutesPage() {
+  return (
+    <div>
+      <Minutes />
+    </div>
+  );
+}
